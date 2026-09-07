5083bebe7ceb2c92123b6650be1b0e931c4065da
venomimonstro <lisencko.art@gmail.com>
2026-09-07 22:57:03 +0300

    Sprint 45: register conversation memory model

--- app/models.py
+++ app/models.py
@@ -24,3 +24,4 @@ from app.models_sprint30 import *  # noqa: F401,F403,E402
 from app.models_sprint31 import *  # noqa: F401,F403,E402
 from app.models_sprint37 import *  # noqa: F401,F403,E402
 from app.models_sprint38 import *  # noqa: F401,F403,E402
+from app.models_sprint45 import *  # noqa: F401,F403,E402
