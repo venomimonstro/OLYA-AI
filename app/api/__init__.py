7d4df35b032bd8edcad3111bd90575705dc7832e
venomimonstro <lisencko.art@gmail.com>
2026-09-14 14:14:22 +0300

    fix(brand): install public model-vendor sanitizer

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -11,6 +11,7 @@ from app.user_surface_finalizer import install_user_surface_finalizer
 from app.workspace_client_v3 import install_workspace_client_v3
 from app.task_solver_access_patch import install_task_solver_access_patch
 from app.source_metadata_patch import install_source_metadata_patch
+from app.public_brand_policy import install_public_brand_policy
 
 install_admin_surface_patch()
 install_product_ui_refresh()
@@ -18,3 +19,4 @@ install_user_surface_finalizer()
 install_workspace_client_v3()
 install_task_solver_access_patch()
 install_source_metadata_patch()
+install_public_brand_policy()
